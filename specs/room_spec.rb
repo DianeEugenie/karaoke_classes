5960edc53b8212e93a837f94c71ff1236d342dc6
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-07-26 12:19:12 +0100

    Add some songs to the room and require in the file

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,10 +1,16 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../room.rb")
+require_relative("../song.rb")
+
 
 class RoomTest < MiniTest::Test
 
   def setup()
+    @song = Song.new("Simply The Best", "Tina Turner")
+    @song2 = Song.new("Bohemian Rhapsody", "Queen")
+    @song3 = Song.new("Afscheid", "Volumia!")
+
     @room = Room.new("Hufflepuff")
     @songs = []
   end
@@ -16,6 +22,7 @@ class RoomTest < MiniTest::Test
   def test_no_songs()
     assert_equal(0, @songs.count)
   end
+
 
 
 end
